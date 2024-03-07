deb791e245a95b8c7654e864eecb68731c98a120
yassin besbes <besbesyassin90@gmail.com>
2024-03-07 11:10:01 +0100

    luste update

--- script.js
+++ script.js
@@ -3,14 +3,16 @@ const yesButton = document.getElementById("yes");
 const noButton = document.getElementById("no");
 const image = document.getElementById("image");
 
-// Add event listeners to the buttons
+// Add event listener to the Yes button
 yesButton.addEventListener("click", function() {
   image.src = "images/love.gif";
   yesButton.style.backgroundColor = "red"; // Change button color to red
 });
 
+
 noButton.addEventListener("click", function() {
   // Change the image source to a different image
   image.src = "images/cry.gif";
 });
+
 
