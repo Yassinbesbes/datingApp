2b683b5603ac8a8c84c54b9f61c98a2960c2d0c8
yassin besbes <besbesyassin90@gmail.com>
2024-03-20 23:59:53 +0100

    new function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Get the elements from the document
 const yesButton = document.getElementById("yes");
-const noButton = document.getElementById("no");
-const image = document.getElementById("image");
+const noButton = document.getElementById("noButton"); // Changed to 'noButton'
+const container = document.getElementById("container");
 
 // Add event listener to the Yes button
 yesButton.addEventListener("click", function() {
@@ -9,10 +9,26 @@ yesButton.addEventListener("click", function() {
   yesButton.style.backgroundColor = "red"; // Change button color to red
 });
 
+function moveButton() {
+  var button = document.getElementById('noButton'); // Changed to 'noButton'
+  var containerRect = container.getBoundingClientRect();
+  
+  var randomX = Math.floor(Math.random() * (containerRect.width - button.offsetWidth));
+  var randomY = Math.floor(Math.random() * (containerRect.height - button.offsetHeight));
+  
+  button.style.left = randomX + 'px';
+  button.style.top = randomY + 'px';
+}
 
-noButton.addEventListener("click", function() {
-  // Change the image source to a different image
-  image.src = "images/cry.gif";
+// Call moveButton function initially to move the button to a random position
+moveButton();
+
+// Add hover event listener to the container
+container.addEventListener("mouseenter", function() {
+  moveButton();
 });
 
-
+// Add click event listener to the "no" button
+noButton.addEventListener("click", function() {
+  container.removeEventListener("mouseenter", moveButton);
+});
