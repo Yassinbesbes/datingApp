357cd929228d90622f6b5e73c6ac850c8e388030
yassin besbes <besbesyassin90@gmail.com>
2024-06-20 00:04:18 +0100

    update some functions

--- script.js
+++ script.js
@@ -1,34 +1,31 @@
-// Get the elements from the document
 const yesButton = document.getElementById("yes");
-const noButton = document.getElementById("noButton"); // Changed to 'noButton'
-const container = document.getElementById("container");
+const noButton = document.getElementById("no");
+const image = document.getElementById("image");
 
-// Add event listener to the Yes button
 yesButton.addEventListener("click", function() {
   image.src = "images/love.gif";
-  yesButton.style.backgroundColor = "red"; // Change button color to red
+  yesButton.style.backgroundColor = "red"; 
+  
 });
 
-function moveButton() {
-  var button = document.getElementById('noButton'); // Changed to 'noButton'
-  var containerRect = container.getBoundingClientRect();
-  
-  var randomX = Math.floor(Math.random() * (containerRect.width - button.offsetWidth));
-  var randomY = Math.floor(Math.random() * (containerRect.height - button.offsetHeight));
-  
-  button.style.left = randomX + 'px';
-  button.style.top = randomY + 'px';
-}
 
-// Call moveButton function initially to move the button to a random position
-moveButton();
-
-// Add hover event listener to the container
-container.addEventListener("mouseenter", function() {
-  moveButton();
-});
-
-// Add click event listener to the "no" button
 noButton.addEventListener("click", function() {
-  container.removeEventListener("mouseenter", moveButton);
+  image.src = "images/cry.gif";
 });
+
+
+
+
+const div = document.querySelector('#no');
+
+div.addEventListener('mouseenter', () => {
+  const viewportWidth = window.innerWidth;
+  const viewportHeight = window.innerHeight;
+  const randomX = Math.random() * (viewportWidth - div.offsetWidth);
+  const randomY = Math.random() * (viewportHeight - div.offsetHeight);
+
+  div.style.position = 'absolute'; 
+  div.style.left = `${randomX}px`;
+  div.style.top = `${randomY}px`;
+});
+
